d6378667b64cb8a2a4674ab1049cc51f0e964243
goodheart <1075623679@qq.com>
2015-08-16 12:45:29 +0800

    初始化framework

---
+++ Category/Category/Category.h
@@ -0,0 +1,19 @@
+//
+//  Category.h
+//  Category
+//
+//  Created by 马健Jane on 15/8/16.
+//  Copyright (c) 2015年 HSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Category.
+FOUNDATION_EXPORT double CategoryVersionNumber;
+
+//! Project version string for Category.
+FOUNDATION_EXPORT const unsigned char CategoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Category/PublicHeader.h>
+
+
